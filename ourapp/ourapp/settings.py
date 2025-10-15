9fe3094666f18aa8b2a2f3f945d6b096ecfca312
notsovish29 <notsovish29@gmail.com>
2025-10-15 13:03:02 +0100

    changes

--- ourapp/ourapp/settings.py
+++ ourapp/ourapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8=%jsg=l*ob21h!og%nceitn_gx!31)z4rk+e!40#2shwym-%%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
